adaf22a904ee58b202d1c8cc27ae36eeaf3bb1dd
ralf kristins <ralf.kristins@yahoo.com>
2020-02-20 18:34:19 +0000

    Add project files.

---
+++ StringEncryption/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringEncryption stringEncryption = new StringEncryption();
+
+            Console.WriteLine("Enter message: ");
+            var userMessage = Console.ReadLine();
+
+            Console.WriteLine("Enter key value: ");
+            var userKey = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Encrypted String:\n" + stringEncryption.EncryptString(userMessage, userKey));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringEncryption/StringEncryption.cs
@@ -0,0 +1,38 @@
+﻿using StringEncryption.Contracts;
+using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEncryption
+{
+    class StringEncryption : IStringEncryption
+    {
+        public String EncryptString(String input, int key)
+        {
+            String returnValue = String.Empty;
+            
+            foreach(char character in input)
+            {
+                returnValue += Cipher(character, key);
+            }
+
+            return returnValue;
+        }
+
+        private Char Cipher(char characters, int key)
+        {
+            if (!char.IsLetter(characters))
+            {
+                return characters;
+            }
+            else
+            {
+                char offset = char.IsUpper(characters) ? 'A' : 'a';
+                return (char)((((characters + key) - offset) % 26) + offset);
+            }
+        }
+    }
+}
